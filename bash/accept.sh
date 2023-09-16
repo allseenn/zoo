d784860520aec04c7aeeb1a9c572fd6eba5215b4
allseenn <allseen@gmail.com>
2023-09-16 23:52:56 +0300

    accept

---
+++ bash/accept.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ]|| [ "$1" == "h" ]; then
+    echo "Формат ввода: create.sh [username]"
+    exit 1;
+fi
+
+if [ $# -ge 1 ]; then
+    USERNAME=$2
+    PAT=$(cat ~/.git-credentials | grep "$USERNAME" | awk -F":" '{ print $3}' | sed 's/@github.com//')
+else
+    PAT=$(cat ~/.git-credentials | awk -F":" '{ print $3}' | sed 's/@github.com//')
+    USERNAME=$(cat ~/.git-credentials | awk -F":" '{ print $2}' | sed  's/\///g')
+fi
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $PAT" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/user/repository_invitations
+
+# curl -L \
+#   -X PATCH \
+#   -H "Accept: application/vnd.github+json" \
+#   -H "Authorization: Bearer <PAT>" \
+#   -H "X-GitHub-Api-Version: 2022-11-28" \
+#   https://api.github.com/user/repository_invitations/232323096
