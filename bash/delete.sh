76f1cdee0eaf4a1d64bc98df150665c0715e7350
allseenn <allseen@gmail.com>
2023-09-17 00:08:40 +0300

    git-credentials

---
+++ bash/delete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Скрипт удаления последней установленной службы, docker swarm и последий созданый образ из системы
+docker service rm $(docker service ls | tail -1 | awk '{ print $2 }')
+docker swarm leave --force
+docker rmi $(docker images | head -2 | tail -1 | awk '{ print $3 }')
