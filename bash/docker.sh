b984355e78709f3e31d2cb9b9b45872bd7d56669
allseenn <allseen@gmail.com>
2023-09-16 20:33:14 +0300

    address

--- bash/docker.sh
+++ bash/docker.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# Проверка наличия аргумента (имя стека)
-if [ $# -ne 1 ]; then
-  echo "Использование: $0 <имя_стека>"
+# Проверка наличия аргументов (имя стека и advertise-addr)
+if [ $# -ne 2 ]; then
+  echo "Использование: $0 <имя_стека> <advertise-addr>"
   exit 1
 fi
 
 # Имя стека передается в аргументе $1
 stack_name="$1"
 
+# Адрес advertise передается в аргументе $2
+advertise_addr="$2"
+
 # Перейдем в каталог с Docker Compose файлом
 cd "$(dirname "$0")/../docker"
 
-# Инициализируем Docker Swarm (если еще не инициализирован)
+# Инициализируем Docker Swarm с указанным advertise-addr (если еще не инициализирован)
 if ! docker info | grep -q "Swarm: active"; then
-    docker swarm init
+    docker swarm init --advertise-addr "$advertise_addr"
 fi
 
 # Развертываем службы из docker-compose.yml с указанным именем стека
