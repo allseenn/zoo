64317435454a4c7b7e31674081eefa10e86805a0
strogino <sysop@bitrix24.ru>
2023-09-20 14:31:16 +0000

    13.Class

--- docker/delete.sh
+++ docker/delete.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Скрипт удаления последней установленной службы, docker swarm и последий созданый образ из системы
-docker --force service rm $(docker service ls | tail -1 | awk '{ print $2 }')
-docker --force swarm leave
-docker --force rmi $(docker images | head -2 | tail -1 | awk '{ print $3 }')
+docker service rm $(docker service ls | tail -1 | awk '{ print $2 }')
+docker swarm leave --force 
+docker rmi $(docker images | head -2 | tail -1 | awk '{ print $3 }') --force 
+rm -rf ~/mysql
 
